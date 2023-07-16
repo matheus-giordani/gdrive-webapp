87a24148cc921cdb4f59ae68aa8f44f478edd664
matheus-giordani <giordani@DESKTOP-KJBQBU2>
2023-07-16 13:45:20 -0300

    .

--- public/app.js
+++ public/app.js
@@ -3,9 +3,7 @@ import ConnectionManager from "./src/connectionManager.js";
 import DragAndDropManager from "./src/dragAndDropManager.js";
 import ViewManager from "./src/viewManager.js";
 
-const API_URL = "http://localhost:3000"
-// const API_URL = "https://gdrive-webapi-ew.herokuapp.com/"
-
+const API_URL = process.env.API_URL ?? 'http://localhost:3000'
 const appController = new AppController({
     viewManager: new ViewManager(),
     dragAndDropManager: new DragAndDropManager(),
